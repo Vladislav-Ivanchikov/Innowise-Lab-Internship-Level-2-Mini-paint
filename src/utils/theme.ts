c756c869e3c0e6b1575a89b67bc5ac6244b96f17
Vladislav <sexxrab@gmail.com>
2022-02-14 17:33:50 +0300

    fix1

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,10 +1,5 @@
 export const baseTheme = {
   colors: {
-    primary: "#7986cb",
-    secondary: "#2b2b2b",
-    success: "#4caf50",
-    danger: "#f44336",
-
     bg: "#BDBDC2FF",
     font: "#101010FF",
   },
